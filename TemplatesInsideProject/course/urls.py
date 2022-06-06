9bdc13fbaf73cf40191fdff0bba021090641966b
taufique siddiqui <63937933+tofiksiddiqui@users.noreply.github.com>
2022-06-07 01:24:48 +0530

    Add files via upload

---
+++ TemplatesInsideProject/course/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('courseone/', views.course_one),
+    path('coursetwo/', views.course_two),
+    path('info/', views.information),
+
+]
